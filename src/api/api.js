589855b25d42ac923febbb9780b5145bef04737f
codeinvisible <lamasandesh400@gmail.com>
2023-05-17 19:20:32 +0545

    api url changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:3000/api";
-// export const baseURL = "https://seventhsem.onrender.com/api";
+// export const baseURL = "http://localhost:3000/api";
+export const baseURL = "https://seventhsem.onrender.com/api";
 
 const api = axios.create({
   baseURL,
